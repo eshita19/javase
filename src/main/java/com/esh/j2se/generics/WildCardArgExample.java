4598d4b82cbd4ffaaf06ce594b61f86be620ef0f
emathur <emathur@cisco.com>
2020-01-08 12:22:57 +0530

    Add bounded wildcard

--- src/main/java/com/esh/j2se/generics/WildCardArgExample.java
+++ src/main/java/com/esh/j2se/generics/WildCardArgExample.java
@@ -9,15 +9,19 @@ class MyType<T extends Number>{
 	public  T getVal(){
 		return this.val;
 	}
-	public boolean sameVal(MyType<?> myType2){
+	public boolean sameVal(MyType<?> myType2){ //Wildcard type arg
 		return  this.val == myType2.getVal();
+	}
+	
+	public boolean sameIntegerVal(MyType<? extends Integer> myType2){ // Bounded wildcard type arg
+		return this.val == myType2.getVal();
 	}
 }
 public class WildCardArgExample {
 	public static void main(String[] args) {
 		MyType<Integer> intType = new MyType<>(10);
 		MyType<Float> floatType = new MyType<>(12.5f);
-		//intType = strType; Compilation error. 
 		System.out.println(intType.sameVal(floatType));
+		System.out.println(intType.sameIntegerVal(intType));
 	}
 }
