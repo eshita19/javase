01fbcdb8e71bc5401fc505efa826fa0fe639cebf
emathur <emathur@cisco.com>
2020-01-08 11:33:36 +0530

    Bounded types

---
+++ src/main/java/com/esh/j2se/generics/BoundedTypesExample.java
@@ -0,0 +1,19 @@
+package com.esh.j2se.generics;
+
+class NumeralType<T extends Number>{
+	T val;
+	public NumeralType(T val) {
+		this.val = val;
+	}
+	public Double getDoubleValue(){
+		return this.val.doubleValue();
+	}
+}
+public class BoundedTypesExample {
+	public static void main(String[] args) {
+		NumeralType<Integer> numeralType = new NumeralType<Integer>(10);
+		System.out.println(numeralType.getDoubleValue());
+		NumeralType<Double> doubleType = new NumeralType<Double>(10.4d);
+		System.out.println(doubleType.getDoubleValue());
+	}
+}
